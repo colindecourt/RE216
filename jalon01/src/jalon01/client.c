56234fc58db37aa72cbc4b25d7348d0cf8deb41d
Colin Decourt <decourtcolin@gmail.com>
2018-09-24 19:34:29 +0200

    Ajout du makefile

--- jalon01/src/jalon01/client.c
+++ jalon01/src/jalon01/client.c
@@ -3,14 +3,78 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <error.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
-int main(int argc,char** argv)
-{
+void get_addr_info(const char* port, struct sockaddr_in* serv_addr1) {
+    int portno;
+
+//clean the serv_add structure
+    memset( serv_addr1, '\0', sizeof(struct sockaddr_in));
+
+//cast the port from a string to an int
+    portno   = atoi(port);
+
+//internet family protocol
+    serv_addr1->sin_family = AF_INET;
+
+//we bind to any ip form the host
+    //inet_aton("127.0.0.1", & serv_addr.sin_addr);
+    serv_addr1->sin_addr.s_addr = htonl(INADDR_ANY);
+    //serv_addr->sin_addr->s_addr = htonl("127.0.0.1");
+
+//we bind on the tcp port specified
+    serv_addr1->sin_port = htons(portno);
+}
+
+
+
+
+
+int do_socket(int domain, int type, int protocol) {
+    int sockfd;
+    int yes = 1;
+//create the socket
+    sockfd = socket( domain, type, protocol );
+
+//check for socket validity
+    if (sockfd == -1){
+      fprintf(stderr,"Error : Unvalid socket\n");
+    }
+
+// set socket option, to prevent "already in use" issue when rebooting the server right on
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        fprintf(stderr,"ERROR setting socket options\n");
+
+    return sockfd;
+}
+
+
+
+
+
+void do_connect(int sockfd, const struct sockaddr_in addr) {
+    int res = connect(sockfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+    if (res != 0) {
+      //fprintf(stderr,"Error : Unable to connect\n");
+      perror("error connection : ");
+      exit(EXIT_FAILURE);
+    }
+    printf("connection ok \n");
+}
+
+
+
+
+int main(int argc,char** argv){
 
 
     if (argc != 3)
@@ -20,14 +84,17 @@ int main(int argc,char** argv)
     }
 
 //get address info from the server
-//get_addr_info()
+struct sockaddr_in serv_addr;
+//serv_addr = malloc(sizeof(struct sockaddr_in));
+
+get_addr_info(argv[2], &serv_addr);
 
 
 //get the socket
-//s = do_socket()
+int s = do_socket(AF_INET, SOCK_STREAM, 0);
 
 //connect to remote socket
-//do_connect()
+do_connect(s, serv_addr);
 
 
 //get user input
--- jalon01/src/jalon01/server.c
+++ jalon01/src/jalon01/server.c
@@ -6,11 +6,84 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+
+#include <arpa/inet.h>
+
+
+
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
+
+
+
+
+int do_socket(int domain, int type, int protocol) {
+    int sockfd;
+    int yes = 1;
+//create the socket
+    sockfd = socket( domain, type, protocol );
+
+//check for socket validity
+    if (sockfd == -1){
+      fprintf(stderr,"Error : Unvalid server socket\n");
+    }
+
+// set socket option, to prevent "already in use" issue when rebooting the server right on
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        fprintf(stderr,"ERROR setting server socket options\n");
+
+    return sockfd;
+}
+
+
+
+
+
+void init_serv_addr(const char* port, struct sockaddr_in* serv_addr) {
+
+    int portno;
+
+//clean the serv_add structure
+    memset( serv_addr, '\0', sizeof(struct sockaddr_in));
+
+//cast the port from a string to an int
+    portno   = (int)&port;
+
+//internet family protocol
+    serv_addr->sin_family = AF_INET;
+
+//we bind to any ip form the host
+    serv_addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+
+//we bind on the tcp port specified
+    serv_addr->sin_port = htons(portno);
+
+}
+
+
+void do_bind(int sock, struct sockaddr_in adr){
+
+    int retbind; //retour de la fonction bind()
+
+    retbind = bind (sock, (struct sockaddr *) &adr, sizeof(struct sockaddr_in));
+
+    if (retbind == -1){
+      //fprintf(stderr,"Error : bind failed\n");
+      perror("error bind : ");
+      exit(EXIT_FAILURE);
+    }
+    printf("bind ok \n");
+}
+
+    //bind(sock, (struct sockaddrr *) &saddr_in, sizeof(saddr_in));
+
+
+
+
 
 int main(int argc, char** argv)
 {
@@ -23,19 +96,28 @@ int main(int argc, char** argv)
 
 
     //create the socket, check for validity!
-    //do_socket()
+    int s = do_socket(AF_INET, SOCK_STREAM, 0);
 
 
     //init the serv_add structure
-    //init_serv_addr()
+    struct sockaddr_in serv_addr;
+    init_serv_addr(argv[1], &serv_addr);
 
     //perform the binding
     //we bind on the tcp port specified
-    //do_bind()
+    do_bind(s, serv_addr);
+
+
 
     //specify the socket to be a server socket and listen for at most 20 concurrent client
-    //listen()
+    int backlog = 20;
+    int retlisten;
+    retlisten = listen(s, backlog);
 
+    if (retlisten == -1){
+      fprintf(stderr,"Error : listen failed\n");
+    }
+/*
     for (;;)
     {
 
@@ -50,7 +132,7 @@ int main(int argc, char** argv)
 
         //clean up client socket
     }
-
+*/
     //clean up server socket
 
     return 0;
