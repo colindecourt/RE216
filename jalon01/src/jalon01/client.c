d4abc863196b950ee75f65a2a478ac7f5bbc0f0c
Colin Decourt <decourtcolin@gmail.com>
2018-09-24 20:47:09 +0200

    connexion serveur

--- jalon01/src/jalon01/client.c
+++ jalon01/src/jalon01/client.c
@@ -13,25 +13,25 @@
 #include <arpa/inet.h>
 
 
-void get_addr_info(const char* port, struct sockaddr_in* serv_addr1) {
-    int portno;
+void get_addr_info(const char* port, struct sockaddr_in* serv_addr1, char* ip) {
+  int portno;
 
-//clean the serv_add structure
-    memset( serv_addr1, '\0', sizeof(struct sockaddr_in));
+  //clean the serv_add structure
+  memset( serv_addr1, '\0', sizeof(struct sockaddr_in));
 
-//cast the port from a string to an int
-    portno   = atoi(port);
+  //cast the port from a string to an int
+  portno   = atoi(port);
 
-//internet family protocol
-    serv_addr1->sin_family = AF_INET;
+  //internet family protocol
+  serv_addr1->sin_family = AF_INET;
 
-//we bind to any ip form the host
-    //inet_aton("127.0.0.1", & serv_addr.sin_addr);
-    serv_addr1->sin_addr.s_addr = htonl(INADDR_ANY);
-    //serv_addr->sin_addr->s_addr = htonl("127.0.0.1");
+  //we bind to any ip form the host
+  //inet_aton("127.0.0.1", & serv_addr.sin_addr);
+  serv_addr1->sin_addr.s_addr = inet_addr(ip);
+  //serv_addr->sin_addr->s_addr = htonl("127.0.0.1");
 
-//we bind on the tcp port specified
-    serv_addr1->sin_port = htons(portno);
+  //we bind on the tcp port specified
+  serv_addr1->sin_port = htons(portno);
 }
 
 
@@ -39,22 +39,22 @@ void get_addr_info(const char* port, struct sockaddr_in* serv_addr1) {
 
 
 int do_socket(int domain, int type, int protocol) {
-    int sockfd;
-    int yes = 1;
-//create the socket
-    sockfd = socket( domain, type, protocol );
+  int sockfd;
+  int yes = 1;
+  //create the socket
+  sockfd = socket( domain, type, protocol );
 
-//check for socket validity
-    if (sockfd == -1){
-      fprintf(stderr,"Error : Unvalid socket\n");
-    }
+  //check for socket validity
+  if (sockfd == -1){
+    fprintf(stderr,"Error : Unvalid socket\n");
+  }
 
-// set socket option, to prevent "already in use" issue when rebooting the server right on
+  // set socket option, to prevent "already in use" issue when rebooting the server right on
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-        fprintf(stderr,"ERROR setting socket options\n");
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+  fprintf(stderr,"ERROR setting socket options\n");
 
-    return sockfd;
+  return sockfd;
 }
 
 
@@ -62,13 +62,13 @@ int do_socket(int domain, int type, int protocol) {
 
 
 void do_connect(int sockfd, const struct sockaddr_in addr) {
-    int res = connect(sockfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
-    if (res != 0) {
-      //fprintf(stderr,"Error : Unable to connect\n");
-      perror("error connection : ");
-      exit(EXIT_FAILURE);
-    }
-    printf("connection ok \n");
+  int res = connect(sockfd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
+  if (res != 0) {
+    //fprintf(stderr,"Error : Unable to connect\n");
+    perror("error connection : ");
+    exit(EXIT_FAILURE);
+  }
+  printf("connection ok \n");
 }
 
 
@@ -76,35 +76,35 @@ void do_connect(int sockfd, const struct sockaddr_in addr) {
 
 int main(int argc,char** argv){
 
+  char * ip = argv[1];
+  if (argc != 3)
+  {
+    fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
+    return 1;
+  }
 
-    if (argc != 3)
-    {
-        fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
-        return 1;
-    }
+  //get address info from the server
+  struct sockaddr_in serv_addr;
+  //serv_addr = malloc(sizeof(struct sockaddr_in));
 
-//get address info from the server
-struct sockaddr_in serv_addr;
-//serv_addr = malloc(sizeof(struct sockaddr_in));
-
-get_addr_info(argv[2], &serv_addr);
-
-
-//get the socket
-int s = do_socket(AF_INET, SOCK_STREAM, 0);
-
-//connect to remote socket
-do_connect(s, serv_addr);
+  get_addr_info(argv[2], &serv_addr, ip);
 
 
-//get user input
-//readline()
+  //get the socket
+  int s = do_socket(AF_INET, SOCK_STREAM, 0);
 
-//send message to the server
-//handle_client_message()
+  //connect to remote socket
+  do_connect(s, serv_addr);
 
 
-    return 0;
+  //get user input
+  //readline()
+
+  //send message to the server
+  //handle_client_message()
+
+
+  return 0;
 
 
 }
--- jalon01/src/jalon01/server.c
+++ jalon01/src/jalon01/server.c
@@ -13,30 +13,30 @@
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(1);
+  perror(msg);
+  exit(1);
 }
 
 
 
 
 int do_socket(int domain, int type, int protocol) {
-    int sockfd;
-    int yes = 1;
-//create the socket
-    sockfd = socket( domain, type, protocol );
+  int sockfd;
+  int yes = 1;
+  //create the socket
+  sockfd = socket( domain, type, protocol );
 
-//check for socket validity
-    if (sockfd == -1){
-      fprintf(stderr,"Error : Unvalid server socket\n");
-    }
+  //check for socket validity
+  if (sockfd == -1){
+    fprintf(stderr,"Error : Unvalid server socket\n");
+  }
 
-// set socket option, to prevent "already in use" issue when rebooting the server right on
+  // set socket option, to prevent "already in use" issue when rebooting the server right on
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-        fprintf(stderr,"ERROR setting server socket options\n");
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+  fprintf(stderr,"ERROR setting server socket options\n");
 
-    return sockfd;
+  return sockfd;
 }
 
 
@@ -45,95 +45,137 @@ int do_socket(int domain, int type, int protocol) {
 
 void init_serv_addr(const char* port, struct sockaddr_in* serv_addr) {
 
-    int portno;
+  int portno;
 
-//clean the serv_add structure
-    memset( serv_addr, '\0', sizeof(struct sockaddr_in));
+  //clean the serv_add structure
+  memset( serv_addr, '\0', sizeof(struct sockaddr_in));
 
-//cast the port from a string to an int
-    portno   = (int)&port;
+  //cast the port from a string to an int
+  portno   = (int)&port;
 
-//internet family protocol
-    serv_addr->sin_family = AF_INET;
+  //internet family protocol
+  serv_addr->sin_family = AF_INET;
 
-//we bind to any ip form the host
-    serv_addr->sin_addr.s_addr = inet_addr("127.0.0.1");
+  //we bind to any ip form the host
+  serv_addr->sin_addr.s_addr = INADDR_ANY;
 
-//we bind on the tcp port specified
-    serv_addr->sin_port = htons(portno);
+  //we bind on the tcp port specified
+  serv_addr->sin_port = htons(portno);
 
 }
 
 
 void do_bind(int sock, struct sockaddr_in adr){
 
-    int retbind; //retour de la fonction bind()
+  int retbind; //retour de la fonction bind()
 
-    retbind = bind (sock, (struct sockaddr *) &adr, sizeof(struct sockaddr_in));
+  retbind = bind (sock, (struct sockaddr *) &adr, sizeof(struct sockaddr_in));
 
-    if (retbind == -1){
-      //fprintf(stderr,"Error : bind failed\n");
-      perror("error bind : ");
-      exit(EXIT_FAILURE);
-    }
-    printf("bind ok \n");
+  if (retbind == -1){
+    //fprintf(stderr,"Error : bind failed\n");
+    perror("error bind : ");
+    exit(EXIT_FAILURE);
+  }
+  printf("bind ok \n");
 }
 
-    //bind(sock, (struct sockaddrr *) &saddr_in, sizeof(saddr_in));
+//bind(sock, (struct sockaddrr *) &saddr_in, sizeof(saddr_in));
 
+//Accept the connection
 
+int do_accept(int sock, struct sockaddr_in adr){
+    int connection = accept(sock,(struct sockaddr *)&adr,(socklen_t*) &adr);
+    if (connection == -1)
+        error("accept ERROR\n");
+    else if (connection > 0)
+        printf("Connection made\n");
+    return connection;
+}
+
+//Read message
+
+ssize_t do_read(int sock, void *data_ptr, size_t to_recv){
+    int recv = 0;
+    do{
+      recv += read(sock, &sock + recv, to_recv - recv);
+
+    } while(recv != to_recv);
+
+    if (recv == -1){
+      error("Data not read");
+    }
+    else{
+      return recv;
+    }
+}
+
+ssize_t do_write(int sock, void *data_ptr, size_t to_send){
+  int sent = 0;
+  do{
+    sent += write(sock, &sock + sent, to_send - sent);
+  } while(sent != to_send);
+
+  if (sent == -1){
+    error("Data not send");
+  }
+  else {
+    return sent;
+  }
+}
 
 
 
 int main(int argc, char** argv)
 {
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "usage: RE216_SERVER port\n");
-        return 1;
-    }
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: RE216_SERVER port\n");
+    return 1;
+  }
 
 
-    //create the socket, check for validity!
-    int s = do_socket(AF_INET, SOCK_STREAM, 0);
+  //create the socket, check for validity!
+  int s = do_socket(AF_INET, SOCK_STREAM, 0);
+  //create message pointer
+  char msg[10000];
+  memset(msg,0,sizeof(msg));
+
+  //init the serv_add structure
+  struct sockaddr_in serv_addr;
+  init_serv_addr(argv[1], &serv_addr);
+
+  //perform the binding
+  //we bind on the tcp port specified
+  do_bind(s, serv_addr);
 
 
-    //init the serv_add structure
-    struct sockaddr_in serv_addr;
-    init_serv_addr(argv[1], &serv_addr);
 
-    //perform the binding
-    //we bind on the tcp port specified
-    do_bind(s, serv_addr);
+  //specify the socket to be a server socket and listen for at most 20 concurrent client
+  int backlog = 20;
+  int retlisten;
+  retlisten = listen(s, backlog);
+  printf("%i\n",retlisten );
+  if (retlisten == -1){
+    fprintf(stderr,"Error : listen failed\n");
+  }
 
+  for (;;)
+  {
 
+  //accept connection from client
+  do_accept(s, serv_addr);
 
-    //specify the socket to be a server socket and listen for at most 20 concurrent client
-    int backlog = 20;
-    int retlisten;
-    retlisten = listen(s, backlog);
+  //read what the client has to say
+  //do_read(sock,msg,)
 
-    if (retlisten == -1){
-      fprintf(stderr,"Error : listen failed\n");
-    }
-/*
-    for (;;)
-    {
+  //we write back to the client
+  //do_write()
 
-        //accept connection from client
-        //do_accept()
+  //clean up client socket
+}
 
-        //read what the client has to say
-        //do_read()
+//clean up server socket
 
-        //we write back to the client
-        //do_write()
-
-        //clean up client socket
-    }
-*/
-    //clean up server socket
-
-    return 0;
+return 0;
 }
